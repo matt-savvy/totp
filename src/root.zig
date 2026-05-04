35c6a7cbe0eed01db5d4bddc449d4b372366cff9
Matt Savoia <matt@1-800-rad-dude.com>
2026-05-03 21:26:05 -0400

    WIP

--- src/root.zig
+++ src/root.zig
@@ -1,18 +1,47 @@
-//! By convention, root.zig is the root source file when making a package.
 const std = @import("std");
+const testing = std.testing;
 const Io = std.Io;
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
+// totp calls hotp
+// hotp needs truncate
+// truncate
+
+/// Step 2: Generate a 4-byte string (Dynamic Truncation)
+///    Let Sbits = DT(HS)   //  DT, defined below,
+///                         //  returns a 31-bit string
 ///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
+///    Step 3: Compute an HOTP value
+///    Let Snum  = StToNum(Sbits)   // Convert S to a number in
+///                                     0...2^{31}-1
+///    Return D = Snum mod 10^Digit //  D is a number in the range
+///                                     0...10^{Digit}-1
+fn truncate(_: [20]u8) [4]u8 {
+    const masked_value: u8 = 0x5a & 0xf;
+    std.debug.print("{d}\n", .{masked_value});
+    // Let OffsetBits be the low-order 4 bits of String[19]
+    // const offset_bits = string[0..4];
+    // Offset = StToNum(OffsetBits) // 0 <= OffSet <= 15
+    // StToNum takes binary and returns a base 10.
+    // const offset =
+    const output = [4]u8{0, 0, 0, 0};
+    return output;
+}
+//
+// test truncate {
+//     const input_1 = [20]u8{ 5, 23, 56, 4, 11, 99, 121, 195, 202, 1, 23, 33, 55, 12, 50, 61, 234, 0, 7, 19 };
+//     const result_1 = truncate(input_1);
+//     try testing.expectEqual(result_1, [4]u8{0, 0, 0, 0});
+// }
+
+fn htop(_: [20]u8, _: u32) u32 {
+    const result = 872921;
+    return result;
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+test htop {
+    const digit = 6;
+    const input = [20]u8{ 0x1f, 0x86, 0x98, 0x69, 0x0e, 0x02, 0xca, 0x16, 0x61, 0x85, 0x50, 0xef, 0x7f, 0x19, 0xda, 0x8e, 0x94, 0x5b, 0x55, 0x5a};
+    const result = htop(input, digit);
+    try testing.expectEqual(872921, result);
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
 }
