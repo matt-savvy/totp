79278c65e6a8c9d28c1d0a19824052de86a0462e
Matt Savoia <matt@1-800-rad-dude.com>
2026-05-06 18:58:59 -0400

    Add stToNum

--- src/root.zig
+++ src/root.zig
@@ -38,10 +38,25 @@ fn htop(_: [20]u8, _: u32) u32 {
     return result;
 }
 
+fn stToNum(input: []const u1) u32 {
+    var result: u32 = 0;
+    var shift_count: u3 = 0;
+    for (input) |byte| {
+        const casted_byte: u32 = byte;
+        result |= (casted_byte << (2 - shift_count));
+        shift_count += 1;
+    }
+    return result;
+}
+
+test stToNum {
+    const st = [_]u1{ 1, 1, 0 };
+    try testing.expectEqual(6, stToNum(&st));
+}
+
 test htop {
     const digit = 6;
     const input = [20]u8{ 0x1f, 0x86, 0x98, 0x69, 0x0e, 0x02, 0xca, 0x16, 0x61, 0x85, 0x50, 0xef, 0x7f, 0x19, 0xda, 0x8e, 0x94, 0x5b, 0x55, 0x5a};
     const result = htop(input, digit);
     try testing.expectEqual(872921, result);
-
 }
