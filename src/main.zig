9b28368664e47f61f58ca8342786846da80f3ecf
Matt Savoia <matt@1-800-rad-dude.com>
2026-05-10 11:25:23 -0400

    Update main, add TODO

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,7 @@ pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const now = Io.Timestamp.now(io, .real);
     const otp = Totp.totp(secret, now, .{});
+    // TODO write to std out without otp
     std.debug.print("otp: {d}\n", .{otp});
     return;
 }
