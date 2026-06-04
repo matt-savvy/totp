eb81acd69f9225ed940edde52422004cff8dfbb7
Matt Savoia <matt@1-800-rad-dude.com>
2026-06-04 18:14:15 -0400

    Refactor decode32, simplify output_idx

--- src/base32.zig
+++ src/base32.zig
@@ -76,11 +76,10 @@ pub fn decode(input: []const u8, allocator: std.mem.Allocator) ![]u8 {
         var bytes: [5]u8 = @bitCast(eight_fives);
         std.mem.reverse(u8, &bytes);
 
-        for (bytes, output_idx..) |byte, i| {
-            output_buf[i] = byte;
+        for (bytes) |byte| {
+            output_buf[output_idx] = byte;
+            output_idx += 1;
         }
-
-        output_idx += 5;
     }
     const output = try allocator.alloc(u8, output_idx);
     @memcpy(output, output_buf[0..output_idx]);
